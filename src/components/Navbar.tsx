9a724a669b0f17a2604f7e9f03866d3817d5b4fb
sailikhithk <sailikhithcse@gmail.com>
2024-07-26 11:51:49 -0500

    updated

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,7 +4,7 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './Navbar.css';
 import logo from '../assets/Logo.jpg';
-import { FaBars, FaTimes } from 'react-icons/fa';
+import { FaBars, FaTimes, FaUser, FaCog, FaBell } from 'react-icons/fa';
 
 const Navbar: React.FC = () => {
   const [isOpen, setIsOpen] = useState<boolean>(false);
@@ -26,16 +26,20 @@ const Navbar: React.FC = () => {
           <li><Link to="/biddingprocess" onClick={toggleMenu}>Bidding</Link></li>
           <li><Link to="/schemes" onClick={toggleMenu}>Government Schemes</Link></li>
           <li><Link to="/knowledge" onClick={toggleMenu}>Knowledge Hub</Link></li>
-          <li><Link to="/login" onClick={toggleMenu}>Login</Link></li>
-          <li><Link to="/signup" onClick={toggleMenu}>Sign Up</Link></li>
         </ul>
-        <div className="header-icons">
-          <span className="icon">👤</span>
-          <span className="icon">⚙️</span>
-          <span className="icon">🔔</span>
-        </div>
-        <div className="menu-icon" onClick={toggleMenu}>
-          {isOpen ? <FaTimes /> : <FaBars />}
+        <div className="navbar-actions">
+          <div className="header-icons">
+            <FaUser className="icon" />
+            <FaCog className="icon" />
+            <FaBell className="icon" />
+          </div>
+          <div className="auth-links">
+            <Link to="/login" onClick={toggleMenu}>Login</Link>
+            <Link to="/signup" onClick={toggleMenu}>Sign Up</Link>
+          </div>
+          <div className="menu-icon" onClick={toggleMenu}>
+            {isOpen ? <FaTimes /> : <FaBars />}
+          </div>
         </div>
       </div>
     </nav>
