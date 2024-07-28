1e1e1f5dd2925a4f97069d4b502b22f5fb11f199
sailikhithk <sailikhithcse@gmail.com>
2024-07-27 22:59:21 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,12 @@
 
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Navbar from './components/Navbar';
 import HomePage from './components/HomePage';
 import SoilHealthMonitoring from './components/SoilHealthMonitoring';
 import BiddingProcess from './components/BiddingProcess';
 import SignupPage from './components/SignupPage';
 import LoginPage from './components/LoginPage';
+import Profile from './components/Profile';
 import './App.css';
 
 function App() {
@@ -20,6 +20,7 @@ function App() {
           <Route path="/biddingprocess" element={<BiddingProcess />} />
           <Route path="/signup" element={<SignupPage />} />
           <Route path="/login" element={<LoginPage />} />
+          <Route path="/profile" element={<Profile />} />
         </Routes>
       </div>
     </Router>
