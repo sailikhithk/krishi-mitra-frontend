93d961a4d0a8b3b09d2be3184fd273b67ee1a768
sailikhithk <sailikhithcse@gmail.com>
2024-07-28 22:39:01 -0500

    updated IMAGES

--- src/components/SoilHealthMonitoring.tsx
+++ src/components/SoilHealthMonitoring.tsx
@@ -11,6 +11,15 @@ import BarCharts from "./BarCharts";
 import AreaCharts from "./AreaChart";
 import LineCharts from "./LineChart";
 
+// Import images
+import farmImage from '../assets/farm.jpg';
+import capsicumImage from '../assets/bellpepper.webp';
+import chilliImage from '../assets/chilli.webp';
+import spinachImage from '../assets/spinach.webp';
+import tomatoImage from '../assets/tomato.webp';
+import potatoImage from '../assets/potato.webp';
+import radishImage from '../assets/radish.webp';
+
 ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend);
 
 const SoilHealthMonitoring = () => {
@@ -90,13 +99,13 @@ const SoilHealthMonitoring = () => {
   };
 
   const soiltestingresults = [
-    { image: "path_to_image1.jpg", title: "Capsicum" },
-    { image: "path_to_image1.jpg", title: "Chilli" },
-    { image: "path_to_image1.jpg", title: "Bell pepper" },
-    { image: "path_to_image1.jpg", title: "Spinach" },
-    { image: "path_to_image1.jpg", title: "Tomato" },
-    { image: "path_to_image1.jpg", title: "Potato" },
-    { image: "path_to_image1.jpg", title: "Radish" },
+    { image: capsicumImage, title: "Capsicum" },
+    { image: chilliImage, title: "Chilli" },
+    { image: capsicumImage, title: "Bell pepper" },
+    { image: spinachImage, title: "Spinach" },
+    { image: tomatoImage, title: "Tomato" },
+    { image: potatoImage, title: "Potato" },
+    { image: radishImage, title: "Radish" },
   ];
 
   const renderMetricDetails = () => {
@@ -189,7 +198,7 @@ const SoilHealthMonitoring = () => {
       <div className="content">
         {!showHistory ? (
           <>
-            <div className="farm-image"></div>
+            <div className="farm-image" style={{backgroundImage: `url(${farmImage})`}}></div>
 
             <div className="soil-metrics">
               {metrics.map((metric) => (
